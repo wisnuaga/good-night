56434681f94047e719678cdee4ed360786ef986e
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-21 02:57:52 +0700

    implement relational on user and follower

--- app/models/follower.rb
+++ app/models/follower.rb
@@ -1,4 +1,6 @@
 class Follower < ApplicationRecord
   belongs_to :user
-  belongs_to :follower
+  belongs_to :follower, class_name: "User"
+
+  validates :user_id, uniqueness: { scope: :follower_id }
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+  # users I follow
+  has_many :active_follows, class_name: "Follower", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_follows, source: :user
+
+  # users who follow me
+  has_many :passive_follows, class_name: "Follower", foreign_key: "user_id", dependent: :destroy
+  has_many :followers, through: :passive_follows, source: :follower
 end
