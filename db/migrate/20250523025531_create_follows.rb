ee303617c39af7228e31731576f62d692d31c905
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-23 09:58:15 +0700

    assign foreign key for follower and followed fields

--- db/migrate/20250523025531_create_follows.rb
+++ db/migrate/20250523025531_create_follows.rb
@@ -1,9 +1,8 @@
 class CreateFollows < ActiveRecord::Migration[7.2]
   def change
-    create_table :follows do |t|
-      t.references :follower, null: false, foreign_key: true
-      t.references :followed, null: false, foreign_key: true
-
+    create_table :follows, id: false do |t|
+      t.references :follower, null: false, foreign_key: { to_table: :users }
+      t.references :followed, null: false, foreign_key: { to_table: :users }
     end
   end
 end
