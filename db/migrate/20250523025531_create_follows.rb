7bcb0ffc50eb236a4aff3a6c6517a937db697718
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-23 09:58:38 +0700

    add created_at field

--- db/migrate/20250523025531_create_follows.rb
+++ db/migrate/20250523025531_create_follows.rb
@@ -3,6 +3,7 @@ class CreateFollows < ActiveRecord::Migration[7.2]
     create_table :follows, id: false do |t|
       t.references :follower, null: false, foreign_key: { to_table: :users }
       t.references :followed, null: false, foreign_key: { to_table: :users }
+      t.datetime :created_at, null: false, default: -> { 'CURRENT_TIMESTAMP' }
     end
   end
 end
