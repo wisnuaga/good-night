17557c5ae47e4dffbb5b214c05cccf4f47a369d6
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-23 16:17:42 +0700

    Revert "move current user to base controller"
    This reverts commit 8964f7052980ea6728a512ba77a574197eabf740.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,4 @@
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
-
-  private
-
-  def set_current_user
-    user_id = request.headers["X-User-Id"]
-    render json: { error: "X-User-Id header missing" }, status: :unauthorized and return unless user_id.present?
-
-    @current_user = User.find_by(id: user_id)
-    render json: { error: "Current user not found" }, status: :unauthorized and return if @current_user.nil?
-  end
 end
