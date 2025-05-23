650ffbd6a2afbb5655aec6499bbbd5fb96a2e43e
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-23 10:01:05 +0700

    Update relation on user table

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,9 @@
 class User < ApplicationRecord
-  # users I follow
-  has_many :active_follows, class_name: "Follower", foreign_key: "follower_id", dependent: :destroy
-  has_many :following, through: :active_follows, source: :user
+  # Users I follow
+  has_many :active_follows, class_name: "Follow", foreign_key: "follower_id", dependent: :destroy
+  has_many :following, through: :active_follows, source: :followed
 
-  # users who follow me
-  has_many :passive_follows, class_name: "Follower", foreign_key: "user_id", dependent: :destroy
+  # Users who follow me
+  has_many :passive_follows, class_name: "Follow", foreign_key: "followed_id", dependent: :destroy
   has_many :followers, through: :passive_follows, source: :follower
 end
