c87b9ddb53670e41611fe6439b713e4ecce35520
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-25 01:48:38 +0700

    Implement reusable function on sleep record controllers

--- app/controllers/sleep_record_controller.rb
+++ app/controllers/sleep_record_controller.rb
@@ -5,29 +5,35 @@ class SleepRecordController < ApplicationController
   # POST /sleep_records/clock_in
   def clock_in
     result = SleepRecordUsecase::ClockIn.new(@current_user).call
-    if result.success?
-      render json: result.data, status: :created
-    else
-      render json: { error: result.error }, status: :bad_request
-    end
+    render_result(result, :created)
   end
 
   # PUT /sleep_records/clock_out
   def clock_out
     result = SleepRecordUsecase::ClockOut.new(@current_user).call
-    if result.success?
-      render json: result.data, status: :ok
-    else
-      render json: { error: result.error }, status: :bad_request
-    end
+    render_result(result, :ok)
   end
 
   def index
-    include_followers = params[:include_followers] == "true"
+    result = SleepRecordUsecase::List.new(
+      @current_user,
+      include_followers: index_params[:include_followers]
+    ).call
 
-    result = SleepRecordUsecase::List.new(@current_user, include_followers: include_followers).call
+    render_result(result, :ok)
+  end
+
+  private
+
+  def index_params
+    params.permit(:include_followers).tap do |params|
+      params[:include_followers] = params[:include_followers] == "true"
+    end
+  end
+
+  def render_result(result, success_status)
     if result.success?
-      render json: result.data, status: :ok
+      render json: result.data, status: success_status
     else
       render json: { error: result.error }, status: :bad_request
     end
