3f5fe621c4f44a053e1722a50492ce578a2b3ce9
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-24 23:32:28 +0700

    add clock-out controller

--- app/controllers/sleep_record_controller.rb
+++ app/controllers/sleep_record_controller.rb
@@ -2,7 +2,7 @@ class SleepRecordController < ApplicationController
   skip_before_action :verify_authenticity_token
   before_action :set_current_user
 
-  # POST /sleep-records/sleep
+  # POST /sleep_records/clock_in
   def clock_in
     # Check if an active session exists
     active_session = @current_user.sleep_records.find_by(clock_out: nil)
@@ -13,6 +13,21 @@ class SleepRecordController < ApplicationController
     sleep_record = @current_user.sleep_records.new(clock_in: Time.current)
     if sleep_record.save
       render json: sleep_record, status: :created
+    else
+      render json: { errors: sleep_record.errors.full_messages }, status: :unprocessable_entity
+    end
+  end
+
+  # PUT /sleep_records/clock_out
+  def clock_out
+    sleep_record = @current_user.sleep_records.where(clock_out: nil).order(:clock_in).last
+    if sleep_record.nil?
+      return render json: { error: "No active sleep session found" }, status: :not_found
+    end
+
+    sleep_record.clock_out = Time.current
+    if sleep_record.save
+      render json: sleep_record, status: :ok
     else
       render json: { errors: sleep_record.errors.full_messages }, status: :unprocessable_entity
     end
