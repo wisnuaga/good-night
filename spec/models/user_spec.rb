b38596c4f12c1ee3f5e89943acbc1079f17a281d
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-25 23:56:13 +0700

    model/user: update UT

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -9,5 +9,28 @@ RSpec.describe User, type: :model do
   it "is invalid without a name" do
     user = User.new(name: nil)
     expect(user).not_to be_valid
+    expect(user.errors[:name]).to include("can't be blank")
+  end
+
+  describe "follower/following associations" do
+    let(:alice) { User.create!(name: "Alice") }
+    let(:bob) { User.create!(name: "Bob") }
+
+    before do
+      # alice follows bob
+      alice.active_follows.create!(followee: bob)
+    end
+
+    it "allows a user to follow another user" do
+      expect(alice.following).to include(bob)
+    end
+
+    it "shows followers of a user" do
+      expect(bob.followers).to include(alice)
+    end
+
+    it "destroys follows when user is deleted" do
+      expect { alice.destroy }.to change { Follow.count }.by(-1)
+    end
   end
 end
