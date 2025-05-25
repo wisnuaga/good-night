6772b50075fa38ee44cf022291d555b3e70b2f4f
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 02:17:18 +0700

    implement UT for list sleep record fanout usage

--- spec/usecases/sleep_record_usecase/list_spec.rb
+++ spec/usecases/sleep_record_usecase/list_spec.rb
@@ -8,23 +8,45 @@ RSpec.describe SleepRecordUsecase::List do
   context "without followees" do
     let(:usecase) { described_class.new(user, sleep_record_repository: sleep_record_repo, follow_repository: follow_repo) }
 
-    it "lists records for only the user" do
+    it "falls back to list_by_user_ids when fanout is empty" do
+      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([])
       expect(sleep_record_repo).to receive(:list_by_user_ids).with([1]).and_return([:record])
+
       result = usecase.call
       expect(result.success?).to be true
       expect(result.data[:data]).to eq([:record])
+    end
+
+    it "returns fanout results if present" do
+      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([:fanout_record])
+
+      result = usecase.call
+      expect(result.success?).to be true
+      expect(result.data[:data]).to eq([:fanout_record])
     end
   end
 
   context "with followees" do
     let(:usecase) { described_class.new(user, sleep_record_repository: sleep_record_repo, follow_repository: follow_repo, include_followees: true) }
 
-    it "lists records for user and followees" do
-      allow(follow_repo).to receive(:list_followee_ids).with(follower_id: user.id).and_return([2, 3])
+    it "falls back to list_by_user_ids for user and followees when fanout is empty" do
+      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([])
+      expect(follow_repo).to receive(:list_followee_ids).with(user_id: user.id).and_return([2, 3])
       expect(sleep_record_repo).to receive(:list_by_user_ids).with([2, 3, 1]).and_return([:record])
+
       result = usecase.call
       expect(result.success?).to be true
       expect(result.data[:data]).to eq([:record])
+    end
+
+    it "returns fanout results if present, without calling list_by_user_ids" do
+      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([:fanout_record])
+      expect(follow_repo).not_to receive(:list_followee_ids)
+      expect(sleep_record_repo).not_to receive(:list_by_user_ids)
+
+      result = usecase.call
+      expect(result.success?).to be true
+      expect(result.data[:data]).to eq([:fanout_record])
     end
   end
 end
