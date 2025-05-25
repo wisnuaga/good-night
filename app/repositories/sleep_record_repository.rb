86f114b462d8b5710a69f2201aa63568a02778dd
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 01:08:52 +0700

    implement fanout to followers on repository layer

--- app/repositories/sleep_record_repository.rb
+++ app/repositories/sleep_record_repository.rb
@@ -19,4 +19,17 @@ class SleepRecordRepository
   def delete(sleep_record)
     sleep_record.destroy
   end
+
+  def fanout_to_followers(sleep_record, follower_ids)
+    follower_ids.each do |follower_id|
+      $redis.lpush(feed_key(follower_id), sleep_record.to_json)
+      $redis.ltrim(feed_key(follower_id), 0, FEED_LIST_LIMIT - 1)
+    end
+  end
+
+  private
+
+  def feed_key(follower_id)
+    "feed:#{follower_id}"
+  end
 end
