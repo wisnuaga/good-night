2fae00a52422f92261061e86321d1cbec1019460
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-25 23:16:07 +0700

    follow usecase base: use user repository instead of directly calling model

--- app/usecases/follow_usecase/base.rb
+++ app/usecases/follow_usecase/base.rb
@@ -2,18 +2,19 @@ require "ostruct"
 
 module FollowUsecase
   class Base
-    def initialize(user, followee_id, follow_repository: FollowRepository.new)
+    def initialize(user, followee_id, follow_repository: FollowRepository.new, user_repository: UserRepository.new)
       @user = user
       @followee_id = followee_id
       @follow_repository = follow_repository
+      @user_repository = user_repository
     end
 
     private
 
-    attr_reader :user, :followee_id, :follow_repository, :followee
+    attr_reader :user, :followee_id, :follow_repository, :user_repository, :followee
 
     def followee
-      @followee ||= User.find_by(id: followee_id)
+      @followee ||= user_repository.find_by_id(id: followee_id)
     end
 
     # Returns OpenStruct success or failure, no exceptions
