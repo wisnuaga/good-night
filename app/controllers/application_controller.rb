43404d8c025e5245b6a82479f970303cdd54cb08
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 00:33:28 +0700

    import ostruct on app controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'ostruct'
+
 class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
