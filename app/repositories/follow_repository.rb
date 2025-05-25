f63ebbb8f75ad84c0004389ca4ac0da77764b5f7
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 01:16:33 +0700

    add function list_follower_ids

--- app/repositories/follow_repository.rb
+++ app/repositories/follow_repository.rb
@@ -14,4 +14,8 @@ class FollowRepository
   def list_followee_ids(follower_id:)
     Follow.where(follower_id: follower_id).pluck(:followee_id)
   end
+
+  def list_follower_ids(user_id:)
+    Follow.where(followee_id: user_id).pluck(:follower_id)
+  end
 end
