c36801fe289cb6dec214e6068af8e617ca8f5301
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-25 22:41:07 +0700

    Add new errors related to session sleep

--- app/errors/usecase_error.rb
+++ app/errors/usecase_error.rb
@@ -4,4 +4,16 @@ class UsecaseError < StandardError
       super
     end
   end
+
+  class ActiveSleepSessionNotFound < UsecaseError
+    def initialize(msg = "No active sleep session found")
+      super
+    end
+  end
+
+  class ActiveSleepSessionAlreadyExists < UsecaseError
+    def initialize(msg = "You already have an active sleep session")
+      super
+    end
+  end
 end
