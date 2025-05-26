9d535922310e9d5f9595dc4f4e0a97106438e696
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 23:15:47 +0700

    update fanout repository UT

--- spec/repositories/fanout_repository_spec.rb
+++ spec/repositories/fanout_repository_spec.rb
@@ -3,99 +3,103 @@ require 'rails_helper'
 RSpec.describe FanoutRepository do
   let(:repo) { described_class.new }
   let(:user) { User.create!(name: "Alice") }
+  let(:follower_ids) { [101, 102, 103] }
 
   before do
     SleepRecord.delete_all
+    stub_const("Repository::FEED_LIST_LIMIT", 2)
+    stub_const("Repository::FEED_TTL_SECONDS", 3600)
+    follower_ids.each { |fid| $redis.del("feed:#{fid}") }
+  end
+
+  def create_sleep_record(offset_hours)
+    SleepRecord.create!(
+      user: user,
+      clock_in: offset_hours.hours.ago,
+      clock_out: (offset_hours - 1).hours.ago
+    )
   end
 
   describe "#write_fanout" do
-    let(:follower_ids) { [101, 102, 103] }
-    let(:sleep_record) do
-      SleepRecord.create!(user: user, clock_in: 2.hours.ago, clock_out: 1.hour.ago)
-    end
+    let(:sleep_record) { create_sleep_record(2) }
 
-    before do
-      stub_const("Repository::FEED_LIST_LIMIT", 2)
-      stub_const("Repository::FEED_TTL_SECONDS", 3600)
-      follower_ids.each { |fid| $redis.del("feed:#{fid}") }
-    end
-
-    it "adds the sleep record to each follower's sorted set feed with correct score" do
+    it "adds the sleep record to each follower's feed with correct score" do
       repo.write_fanout(sleep_record: sleep_record, follower_ids: follower_ids)
 
       follower_ids.each do |fid|
         feed_key = "feed:#{fid}"
-        # Fetch all members with scores from sorted set
-        members_with_scores = $redis.zrange(feed_key, 0, -1, with_scores: true)
+        members = $redis.zrange(feed_key, 0, -1, with_scores: true)
 
-        # There should be exactly one member, the sleep_record.id with score = clock_in.to_i
-        expect(members_with_scores.length).to eq(1)
-        member, score = members_with_scores.first
+        expect(members.length).to eq(1)
+        member, score = members.first
         expect(member.to_i).to eq(sleep_record.id)
         expect(score.to_i).to eq(sleep_record.clock_in.to_i)
-
-        # Check TTL is set (greater than 0)
-        ttl = $redis.ttl(feed_key)
-        expect(ttl).to be > 0
+        expect($redis.ttl(feed_key)).to be > 0
       end
     end
 
-    it "trims the sorted set feed to FEED_LIST_LIMIT most recent items" do
-      # Create 3 sleep records with increasing clock_in times
-      records = 3.times.map do |i|
-        SleepRecord.create!(user: user, clock_in: (3 - i).hours.ago, clock_out: (2 - i).hours.ago)
-      end
-
-      records.each do |sr|
-        repo.write_fanout(sleep_record: sr, follower_ids: follower_ids)
-      end
+    it "trims the feed to FEED_LIST_LIMIT most recent items" do
+      records = [3, 2, 1].map { |h| create_sleep_record(h) }
+      records.each { |sr| repo.write_fanout(sleep_record: sr, follower_ids: follower_ids) }
 
       follower_ids.each do |fid|
-        feed_key = "feed:#{fid}"
-        # zrevrange returns highest score first (most recent clock_in)
-        feed = $redis.zrevrange(feed_key, 0, -1).map(&:to_i)
-
-        # The feed should be trimmed to FEED_LIST_LIMIT (2)
-        expect(feed.length).to eq(2)
-
-        # It should contain the 2 most recent sleep record IDs by clock_in
-        expected_ids = records.sort_by(&:clock_in).reverse.first(2).map(&:id)
-        expect(feed).to eq(expected_ids)
+        feed = $redis.zrevrange("feed:#{fid}", 0, -1).map(&:to_i)
+        expected = records.sort_by(&:clock_in).reverse.first(2).map(&:id)
+        expect(feed).to eq(expected)
       end
+    end
+
+    it "does not add duplicate sleep record to feed" do
+      repo.write_fanout(sleep_record: sleep_record, follower_ids: follower_ids)
+      repo.write_fanout(sleep_record: sleep_record, follower_ids: follower_ids)
+
+      follower_ids.each do |fid|
+        feed = $redis.zrange("feed:#{fid}", 0, -1)
+        expect(feed.count { |id| id.to_i == sleep_record.id }).to eq(1)
+      end
+    end
+
+    it "resets TTL on each addition" do
+      repo.write_fanout(sleep_record: sleep_record, follower_ids: [follower_ids[0]])
+      sleep(2)
+      ttl_1 = $redis.ttl("feed:#{follower_ids[0]}")
+
+      new_record = SleepRecord.create!(user: user, clock_in: 1.hour.ago, clock_out: Time.now)
+      repo.write_fanout(sleep_record: new_record, follower_ids: [follower_ids[1]])
+      ttl_2 = $redis.ttl("feed:#{follower_ids[1]}")
+
+      expect(ttl_2).to be > ttl_1
     end
   end
 
   describe "#list_fanout" do
-    let(:user_id) { 101 }
+    let(:user_id) { follower_ids.first }
     let(:feed_key) { "feed:#{user_id}" }
-    let(:sleep_records) do
-      3.times.map do |i|
-        SleepRecord.create!(user: user, clock_in: (3 - i).hours.ago, clock_out: (2 - i).hours.ago)
-      end
-    end
+    let(:sleep_records) { [3, 2, 1].map { |h| create_sleep_record(h) } }
 
     before do
       $redis.del(feed_key)
-      sleep_records.each do |sr|
-        $redis.zadd(feed_key, sr.clock_in.to_i, sr.id)
-      end
+      sleep_records.each { |sr| $redis.zadd(feed_key, sr.clock_in.to_i, sr.id) }
     end
 
-    it "returns sleep record ids from Redis sorted set in descending order by clock_in" do
+    it "returns sleep record ids in descending order by clock_in" do
       ids = repo.list_fanout(user_id: user_id, limit: 2)
-      expected_ids = sleep_records.sort_by(&:clock_in).reverse.first(2).map(&:id)
-      expect(ids).to eq(expected_ids)
+      expected = sleep_records.sort_by(&:clock_in).reverse.first(2).map(&:id)
+      expect(ids).to eq(expected)
     end
 
-    context "when cache returns empty list" do
-      before do
-        $redis.del(feed_key)  # ensure cache is empty
-      end
+    it "returns items older than cursor when provided" do
+      sorted = sleep_records.sort_by(&:clock_in).reverse
+      cursor = sorted.first.clock_in.to_i
+      ids = repo.list_fanout(user_id: user_id, cursor: cursor, limit: 2)
+      expected = sorted.drop(1).first(2).map(&:id)
+      expect(ids).to eq(expected)
+    end
 
-      it "returns an empty array" do
-        ids = repo.list_fanout(user_id: user_id, limit: 2)
-        expect(ids).to eq([])
-      end
+    it "returns an empty array when cache is empty" do
+      $redis.del(feed_key)
+      ids = repo.list_fanout(user_id: user_id, limit: 2)
+      expect(ids).to eq([])
     end
   end
 end
