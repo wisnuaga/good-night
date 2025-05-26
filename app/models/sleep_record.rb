977701a1c43680a2f5a5cddf2c45b36d6209dff0
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-27 02:44:11 +0700

    add calculate_sleep_time before save

--- app/models/sleep_record.rb
+++ app/models/sleep_record.rb
@@ -7,11 +7,22 @@ class SleepRecord < ApplicationRecord
   # Custom validation to prevent overlapping active sleep records per user
   validate :no_overlapping_active_sessions, on: :create
 
+  # Calculate sleep_time before saving record
+  before_save :calculate_sleep_time
+
   private
 
   def no_overlapping_active_sessions
     if SleepRecord.where(user_id: user_id, clock_out: nil).exists?
       errors.add(:base, "You already have an active sleep session")
+    end
+  end
+
+  def calculate_sleep_time
+    if clock_out.present? && clock_in.present? && clock_out > clock_in
+      self.sleep_time = clock_out - clock_in  # duration in seconds
+    else
+      self.sleep_time = nil
     end
   end
 end
