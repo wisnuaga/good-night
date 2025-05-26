f205ca8964599e2df5501eb0f3eec1463b85cedc
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 17:09:06 +0700

    refactor sleep_record_fanout_job.rb

--- app/jobs/sleep_record_fanout_job.rb
+++ app/jobs/sleep_record_fanout_job.rb
@@ -6,7 +6,7 @@ class SleepRecordFanoutJob < ApplicationJob
     return unless sleep_record
 
     # Use your repository method to fanout cache writes
-    SleepRecordRepository.new.fanout_to_followers(sleep_record: sleep_record, follower_ids: follower_ids)
+    SleepRecordRepository.new.write_fanout(sleep_record: sleep_record, follower_ids: follower_ids)
   rescue => e
     Rails.logger.error("[SleepRecordFanoutJob] Failed for record #{sleep_record_id}: #{e.message}")
   end
