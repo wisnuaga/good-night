fb0cff9c0af58ca01922e7c41e3bfddf48033f71
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-26 12:20:49 +0700

    fix list UT

--- spec/usecases/sleep_record_usecase/list_spec.rb
+++ spec/usecases/sleep_record_usecase/list_spec.rb
@@ -1,52 +1,56 @@
-require "rails_helper"
+require 'rails_helper'
 
 RSpec.describe SleepRecordUsecase::List do
   let(:user) { instance_double("User", id: 1) }
   let(:sleep_record_repo) { instance_double(SleepRecordRepository) }
   let(:follow_repo) { instance_double(FollowRepository) }
 
-  context "without followees" do
-    let(:usecase) { described_class.new(user, sleep_record_repository: sleep_record_repo, follow_repository: follow_repo) }
+  let(:usecase) { described_class.new(user, sleep_record_repository: sleep_record_repo, follow_repository: follow_repo) }
 
-    it "falls back to list_by_user_ids when fanout is empty" do
-      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([])
-      expect(sleep_record_repo).to receive(:list_by_user_ids).with([1]).and_return([:record])
+  # Create a fake record double with clock_in method
+  let(:record1) { instance_double("SleepRecord", clock_in: 1_686_470_000) }
+  let(:record2) { instance_double("SleepRecord", clock_in: 1_686_470_100) }
 
-      result = usecase.call
-      expect(result.success?).to be true
-      expect(result.data[:data]).to eq([:record])
-    end
-
-    it "returns fanout results if present" do
-      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([:fanout_record])
-
-      result = usecase.call
-      expect(result.success?).to be true
-      expect(result.data[:data]).to eq([:fanout_record])
-    end
-  end
-
-  context "with followees" do
-    let(:usecase) { described_class.new(user, sleep_record_repository: sleep_record_repo, follow_repository: follow_repo, include_followees: true) }
-
+  context "general behavior" do
     it "falls back to list_by_user_ids for user and followees when fanout is empty" do
       expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([])
+
       expect(follow_repo).to receive(:list_followee_ids).with(user_id: user.id).and_return([2, 3])
-      expect(sleep_record_repo).to receive(:list_by_user_ids).with([2, 3, 1]).and_return([:record])
+
+      expect(sleep_record_repo).to receive(:list_by_user_ids)
+                                     .with(user_ids: [2, 3, 1], cursor: nil, limit: described_class::CURSOR_LIMIT)
+                                     .and_return([record1, record2])
 
       result = usecase.call
+
       expect(result.success?).to be true
-      expect(result.data[:data]).to eq([:record])
+      expect(result.data[:data]).to eq([record1, record2])
     end
 
     it "returns fanout results if present, without calling list_by_user_ids" do
-      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return([:fanout_record])
-      expect(follow_repo).not_to receive(:list_followee_ids)
-      expect(sleep_record_repo).not_to receive(:list_by_user_ids)
+      # Use doubles that respond to clock_in here as well
+      fanout_records = [
+        instance_double("SleepRecord", clock_in: 1_686_470_200),
+        instance_double("SleepRecord", clock_in: 1_686_470_300),
+        instance_double("SleepRecord", clock_in: 1_686_470_400)
+      ]
+
+      expect(sleep_record_repo).to receive(:list_fanout).with(user_id: user.id).and_return(fanout_records)
+
+      expect(follow_repo).to receive(:list_followee_ids).with(user_id: user.id).and_return([2, 3])
+
+      expect(sleep_record_repo).to receive(:list_by_ids)
+                                     .with(ids: fanout_records, cursor: nil, limit: described_class::CURSOR_LIMIT)
+                                     .and_return(fanout_records)
+
+      expect(sleep_record_repo).to receive(:count_by_user_ids)
+                                     .with(user_ids: [2, 3, 1])
+                                     .and_return(fanout_records.size)
 
       result = usecase.call
+
       expect(result.success?).to be true
-      expect(result.data[:data]).to eq([:fanout_record])
+      expect(result.data[:data]).to eq(fanout_records)
     end
   end
 end
