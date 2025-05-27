93b06e07fb004081f90cb1c5108e69cd8c3dc1b6
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-27 13:27:18 +0700

    init job to remove after unfollow

---
+++ app/jobs/remove_fanout_after_unfollow_job.rb
@@ -0,0 +1,29 @@
+class RemoveFanoutAfterUnfollowJob < ApplicationJob
+  queue_as :default
+
+  def perform(user_id, unfollowed_user_id)
+    # Confirm the user is *still* not following the target
+    return if FollowRepository.new.following?(user_id, unfollowed_user_id)
+
+    sleep_record_repo = SleepRecordRepository.new
+    fanout_repo = FanoutRepository.new
+
+    cursor_time = nil
+
+    loop do
+      records = sleep_record_repo.list_by_user_ids(
+        user_ids: [unfollowed_user_id],
+        cursor: cursor_time,
+        limit: SleepRecordRepository::FEED_LIST_LIMIT
+      )
+
+      break if records.empty?
+
+      record_ids = records.map(&:id)
+      fanout_repo.remove_from_fanout(user_id: user_id, sleep_record_ids: record_ids)
+
+      # Move cursor to last record's sleep_time for next batch
+      cursor_time = records.last.sleep_time
+    end
+  end
+end
