fae379e3371994b4ac592c2cd5f54a99a3603516
Wisnu Agastya <wisnu.aga@gmail.com>
2025-05-28 07:12:42 +0700

    remove debugger

--- app/lib/caches/sleep_record_cache.rb
+++ app/lib/caches/sleep_record_cache.rb
@@ -2,7 +2,6 @@ module Caches
   class SleepRecordCache < Base
     def initialize
       @ttl = ENV.fetch('SLEEP_RECORD_CACHE_TTL', 6.hours.to_i).to_i
-      puts "TTL: #{@ttl}"
       super(prefix: 'cache:sleep_record', ttl: @ttl)
     end
 
