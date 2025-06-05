852188e40c8bfdb8250a5870b19c1037660b0bf5
Wisnu Agastya <wisnu.aga@gmail.com>
2025-06-05 23:56:24 +0700

    Revert "Fix fanout repair job: move out process_sleep_record_in_batch"
    This reverts commit 53912d765e7f817262b19093919b3eceeba5565c.

--- app/jobs/repair_sleep_record_fanout_job.rb
+++ app/jobs/repair_sleep_record_fanout_job.rb
@@ -9,16 +9,18 @@ class RepairSleepRecordFanoutJob < ApplicationJob
     begin
       sleep_record_repo = SleepRecordRepository.new
       fanout_repo = FanoutRepository.new
-      follow_repo = FollowRepository.new
+      follow_repository = FollowRepository.new
 
       existing_ids = fanout_repo.list_fanout(user_id: user_id)
 
       first = true
       followee_cursor = nil
       followee_limit = 10
+      sleep_time = 0.05
 
       loop do
-        followee_ids, followee_cursor = follow_repo.list_followee_ids_batch(
+        # TODO: Improvement -> this query not ordered by most longest sleep_time within last 7 days
+        followee_ids, followee_cursor = follow_repository.list_followee_ids_batch(
           user_id: user_id,
           cursor: followee_cursor,
           limit: followee_limit
@@ -32,12 +34,28 @@ class RepairSleepRecordFanoutJob < ApplicationJob
 
         break if followee_ids.empty?
 
-        process_sleep_record_in_batch(
-          sleep_record_repo: sleep_record_repo,
-          user_id: user_id,
-          followee_ids: followee_ids,
-          existing_ids: existing_ids
-        )
+        correct_records = []
+        cursor_time = nil
+        loop do
+          batch_records = sleep_record_repo.list_by_user_ids(
+            user_ids: followee_ids,
+            cursor: cursor_time
+          )
+
+          break if batch_records.empty?
+
+          correct_records.concat(batch_records)
+          correct_records.uniq!(&:id)
+          cursor_time = batch_records.last&.sleep_time
+
+          missing_records = correct_records.reject { |r| existing_ids.include?(r.id) }
+          missing_records.each do |record|
+            fanout_repo.add_to_feed(user_id: user_id, sleep_record: record)
+          end
+
+          break if cursor_time.nil?
+          sleep(sleep_time)
+        end
 
         break if followee_cursor.nil?
         sleep(sleep_time)
@@ -46,32 +64,6 @@ class RepairSleepRecordFanoutJob < ApplicationJob
       Rails.logger.error("[RepairSleepRecordFanoutJob] Failed for user #{user_id}: #{e.message}")
     ensure
       $redis.del(lock_key)
-    end
-  end
-
-  def process_sleep_record_in_batch(sleep_record_repo:, user_id:, followee_ids:, existing_ids:, batch_size: Repository::FEED_LIST_LIMIT, sleep_time: 0.05)
-    correct_records = []
-    cursor_time = nil
-    loop do
-      batch_records = sleep_record_repo.list_by_user_ids(
-        user_ids: followee_ids,
-        cursor: cursor_time,
-        limit: batch_size
-      )
-
-      break if batch_records.empty?
-
-      correct_records.concat(batch_records)
-      correct_records.uniq!(&:id)
-      cursor_time = batch_records.last&.sleep_time
-
-      missing_records = correct_records.reject { |r| existing_ids.include?(r.id) }
-      missing_records.each do |record|
-        fanout_repo.add_to_feed(user_id: user_id, sleep_record: record)
-      end
-
-      break if cursor_time.nil?
-      sleep(sleep_time)
     end
   end
 end
